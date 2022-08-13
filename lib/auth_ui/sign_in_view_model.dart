d15134c99ce2decbd4ea7ac0a8787d3cd9a80527
shiwamSliceit <shiwam.karn@sliceit.com>
2022-08-13 18:16:25 +0530

    added sign out features

--- lib/auth_ui/sign_in_view_model.dart
+++ lib/auth_ui/sign_in_view_model.dart
@@ -1,7 +1,11 @@
 import 'dart:async';
 
+import 'package:buddyapp/widget/alert_dailog.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+import 'constants/strings.dart';
 
 class SignInViewModel with ChangeNotifier {
   SignInViewModel({required this.auth});
@@ -29,5 +33,32 @@ class SignInViewModel with ChangeNotifier {
     return auth.signInAnonymously();
   }
 
+ /// Todo will add this wherever we make logout button
+  Future<void> _confirmSignOut(
+      BuildContext context, FirebaseAuth firebaseAuth) async {
+    final bool didRequestSignOut = await showAlertDialog(
+      context: context,
+      title: Strings.logout,
+      content: Strings.logoutAreYouSure,
+      cancelActionText: Strings.cancel,
+      defaultActionText: Strings.logout,
+    ) ??
+        false;
+    if (didRequestSignOut == true) {
+      await signOut(context);
+    }
+  }
+
+  Future<void> signOut(BuildContext context) async {
+    try {
+      await auth.signOut();
+    } catch (e) {
+      unawaited(showExceptionAlertDialog(
+        context: context,
+        title: Strings.logoutFailed,
+        exception: e,
+      ));
+    }
+  }
 }
 
